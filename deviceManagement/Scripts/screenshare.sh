17ab79632a42cbc8b54a0d1aa75aa07537015218
Cece Collins <7505271+thecececollins@users.noreply.github.com>
2018-09-06 20:07:10 -0700

    Create screenshare.sh

---
+++ deviceManagement/Scripts/screenshare.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo defaults write /var/db/launchd.db/com.apple.launchd/overrides.plist com.apple.screensharing -dict Disabled -bool false
+sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.screensharing.plist
+
+exit 0
